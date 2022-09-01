8740005bf32ff21f86e68cf9f4d8d21dd60ec4fd
Edward J. Schwartz <eschwartz@cert.org>
2022-09-01 12:15:00 -0400

    Completeness: Improve arithmetic check
    found manually when trying to debug a completeness = 0 problem

--- share/prolog/oorules/rules.pl
+++ share/prolog/oorules/rules.pl
@@ -2788,7 +2788,7 @@ reasonMergeClasses_H(DerivedClass, MethodClass) :-
     factVFTableEntry(DerivedVFTable, VOffset, Method),
     not(purecall(Method)),
     not(factReusedImplementation(Method)),
-    VOffset > BaseSize,
+    VOffset + 1 > BaseSize,
     find(Method, MethodClass),
     iso_dif(DerivedClass, MethodClass),
     % Debugging
