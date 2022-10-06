8122d02f9dac465d21e31b5d398028c19ccf6704
Edward J. Schwartz <eschwartz@cert.org>
2022-10-06 10:16:36 -0400

    VFTableBelongsToClass attempted fix 1
    Ensure that the vftable is not at the start of a directly embedded class.

---
+++ share/prolog/oorules/vfb-tcs/1/all.cc
@@ -0,0 +1,71 @@
+// type for special constructors
+struct special_constructor {};
+struct C0;
+
+struct C1;
+
+struct C0  {
+/* method id 400 */ virtual ~C0 ();
+/* method id 401 */ C0 ();
+C0 (special_constructor);
+};
+struct C1  {
+/* method id 402 */ C1 ();
+C1 (special_constructor);
+C0 v0;
+};
+// definitions
+/* method id 400 */ C0::~C0 () {
+return;
+
+}
+/* method id 401 */ C0::C0 () {
+
+}
+C0::C0 (special_constructor)  {}
+
+/* method id 402 */ C1::C1 () {
+
+}
+C1::C1 (special_constructor)  {}
+
+#include <iostream>
+void check_driver() {
+/*
+class C0    size(4)
+0    +---
+0    | {vfptr}
+4    +---
+*/
+{
+ const C0* obj = new C0(special_constructor());
+ // verify class size
+ const size_t vc_size = sizeof(C0);
+ const size_t layout_size = 4;
+ if (vc_size == layout_size) {
+  std::cerr << "CLASSSIZE CORRECT: C0 " << layout_size << std::endl;
+ } else {
+  std::cerr << "CLASSSIZE INCORRECT: C0 " << vc_size << " " << layout_size << std::endl;
+ }
+ // verify base offset
+}
+/*
+class C1    size(4)
+0    +---
+0    | var0: ClassType{0}
+4    +---
+*/
+{
+ const C1* obj = new C1(special_constructor());
+ // verify class size
+ const size_t vc_size = sizeof(C1);
+ const size_t layout_size = 4;
+ if (vc_size == layout_size) {
+  std::cerr << "CLASSSIZE CORRECT: C1 " << layout_size << std::endl;
+ } else {
+  std::cerr << "CLASSSIZE INCORRECT: C1 " << vc_size << " " << layout_size << std::endl;
+ }
+ // verify base offset
+}
+}
+int main() { check_driver (); }
